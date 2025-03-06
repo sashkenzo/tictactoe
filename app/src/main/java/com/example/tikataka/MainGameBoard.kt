57b38f143c04a04c3bca25beec6417ac45976e9c
Sashkenzo <119334482+Sashkenzo@users.noreply.github.com>
2025-03-06 16:05:41 +0200

    First commit

---
+++ app/src/main/java/com/example/tikataka/MainGameBoard.kt
@@ -0,0 +1,29 @@
+package com.example.tikataka
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainGameBoard : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main_game_board)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        val move = arrayOf('X','O');
+        
+        val btnAbout: Button = findViewById(R.id.button_about);
+        btnAbout.setOnClickListener {
+            val intent = Intent(this, MainAbout::class.java);
+            startActivity(intent);
+        }
+    }
+}
